cf9314d10ecf3c622d4425ddb02ee1b1d6209b31
Valeria Ramirez <valeriaramirezmartinez@gmail.com>
2025-08-25 16:06:27 -0600

    Dibujo 2D
    Código de práctica 02, imagen creada Ying Yang

--- ConfiguracionInicial/Main_Dibujo2D.cpp
+++ ConfiguracionInicial/Main_Dibujo2D.cpp
@@ -1,6 +1,6 @@
-// Valeria Ramirez Martinez
-// Previo 02
-// Fecha de entrega: 16 / Agosto / 2025
+// Práctica 02
+// Valeria Ramírez Martínez 
+// Fecha de entrega: 20/ Agosto/ 2025
 // No. Cuenta: 318063188
 
 
@@ -29,11 +29,11 @@ int main() {
 	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);*/
 
-	GLFWwindow *window = glfwCreateWindow(WIDTH, HEIGHT, "Previo 02 Valeria Ramirez", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Practica 02 Valeria Ramirez", NULL, NULL);
 	glfwSetFramebufferSizeCallback(window, resize);
-	
+
 	//Verificaci�n de errores de creacion  ventana
-	if (window== NULL) 
+	if (window == NULL)
 	{
 		std::cout << "Failed to create GLFW window" << std::endl;
 		glfwTerminate();
@@ -61,27 +61,112 @@ int main() {
 	// Define las dimensiones del viewport
 	//glViewport(0, 0, screenWidth, screenHeight);
 
-    Shader ourShader("Shader/core.vs", "Shader/core.frag");
+	Shader ourShader("Shader/core.vs", "Shader/core.frag");
 
 	// Set up vertex data (and buffer(s)) and attribute pointers
 	float vertices[] = {
-		0.5f,  0.5f, 0.0f,    1.0f,0.0f,0.0f,  // top right
-		0.5f, -0.5f, 0.0f,    1.0f,1.0f,0.0f,  // bottom right
-		-0.5f, -0.5f, 0.0f,   1.0f,0.0f,1.0f,  // bottom left
-		-0.5f,  0.5f, 0.0f,   1.0f,1.0f,0.0f, // top left 
+		// Primera línea
+		-0.20f,  0.70f, 0.0f,   0.0f,0.0f,0.0f,
+		-0.25f,  0.70f, 0.0f,   0.0f,0.0f,0.0f,
+		-0.30f,  0.70f, 0.0f,   0.0f,0.0f,0.0f,
+		-0.35f,  0.70f, 0.0f,   0.0f,0.0f,0.0f,
+
+		// Segunda línea
+		-0.40f,  0.63f, 0.0f,   0.0f,0.0f,0.0f,
+		-0.35f,  0.63f, 0.0f,   0.0f,0.0f,0.0f,
+		-0.30f,  0.63f, 0.0f,   0.0f,0.0f,0.0f,
+		-0.25f,  0.63f, 0.0f,   1.0f,1.0f,1.0f,
+		-0.20f,  0.63f, 0.0f,   1.0f,1.0f,1.0f,
+		-0.15f,  0.63f, 0.0f,   0.0f,0.0f,0.0f,
+
+		// Tercera línea
+		-0.45f,  0.56f, 0.0f,   0.0f,0.0f,0.0f,
+		-0.40f,  0.56f, 0.0f,   0.0f,0.0f,0.0f,
+		-0.35f,  0.56f, 0.0f,   0.0f,0.0f,0.0f,
+		-0.30f,  0.56f, 0.0f,   1.0f,1.0f,1.0f,
+		-0.25f,  0.56f, 0.0f,   1.0f,1.0f,1.0f,
+		-0.20f,  0.56f, 0.0f,   1.0f,1.0f,1.0f,
+		-0.15f,  0.56f, 0.0f,   1.0f,1.0f,1.0f,
+		-0.10f,  0.56f, 0.0f,   0.0f,0.0f,0.0f,
+
+		// Cuarta línea
+		-0.50f,  0.49f, 0.0f,   0.0f,0.0f,0.0f,
+		-0.45f,  0.49f, 0.0f,   0.0f,0.0f,0.0f,
+		-0.40f,  0.49f, 0.0f,   0.0f,0.0f,0.0f,
+		-0.35f,  0.49f, 0.0f,   0.0f,0.0f,0.0f,
+		-0.30f,  0.49f, 0.0f,   1.0f,1.0f,1.0f,
+		-0.25f,  0.49f, 0.0f,   0.0f,0.0f,0.0f,
+		-0.20f,  0.49f, 0.0f,   1.0f,1.0f,1.0f,
+		-0.15f,  0.49f, 0.0f,   1.0f,1.0f,1.0f,
+		-0.10f,  0.49f, 0.0f,   1.0f,1.0f,1.0f,
+		-0.05f,  0.49f, 0.0f,   0.0f,0.0f,0.0f,
+
+		// Quinta línea
+		-0.50f,  0.42f, 0.0f,   0.0f,0.0f,0.0f,
+		-0.45f,  0.42f, 0.0f,   0.0f,0.0f,0.0f,
+		-0.40f,  0.42f, 0.0f,   0.0f,0.0f,0.0f,
+		-0.35f,  0.42f, 0.0f,   0.0f,0.0f,0.0f,
+		-0.30f,  0.42f, 0.0f,   1.0f,1.0f,1.0f,
+		-0.25f,  0.42f, 0.0f,   1.0f,1.0f,1.0f,
+		-0.20f,  0.42f, 0.0f,   1.0f,1.0f,1.0f,
+		-0.15f,  0.42f, 0.0f,   1.0f,1.0f,1.0f,
+		-0.10f,  0.42f, 0.0f,   1.0f,1.0f,1.0f,
+		-0.05f,  0.42f, 0.0f,   0.0f,0.0f,0.0f,
+
+		// Sexta línea
+		-0.50f,  0.35f, 0.0f,   0.0f,0.0f,0.0f,
+		-0.45f,  0.35f, 0.0f,   0.0f,0.0f,0.0f,
+		-0.40f,  0.35f, 0.0f,   0.0f,0.0f,0.0f,
+		-0.35f,  0.35f, 0.0f,   0.0f,0.0f,0.0f,
+		-0.30f,  0.35f, 0.0f,   0.0f,0.0f,0.0f,
+		-0.25f,  0.35f, 0.0f,   1.0f,1.0f,1.0f,
+		-0.20f,  0.35f, 0.0f,   1.0f,1.0f,1.0f,
+		-0.15f,  0.35f, 0.0f,   1.0f,1.0f,1.0f,
+		-0.10f,  0.35f, 0.0f,   1.0f,1.0f,1.0f,
+		-0.05f,  0.35f, 0.0f,   0.0f,0.0f,0.0f,
+
+		// Séptima línea
+		-0.50f,  0.28f, 0.0f,   0.0f,0.0f,0.0f,
+		-0.45f,  0.28f, 0.0f,   0.0f,0.0f,0.0f,
+		-0.40f,  0.28f, 0.0f,   0.0f,0.0f,0.0f,
+		-0.35f,  0.28f, 0.0f,   0.0f,0.0f,0.0f,
+		-0.30f,  0.28f, 0.0f,   0.0f,0.0f,0.0f,
+		-0.25f,  0.28f, 0.0f,   0.0f,0.0f,0.0f,
+		-0.20f,  0.28f, 0.0f,   1.0f,1.0f,1.0f,
+		-0.15f,  0.28f, 0.0f,   1.0f,1.0f,1.0f,
+		-0.10f,  0.28f, 0.0f,   1.0f,1.0f,1.0f,
+		-0.05f,  0.28f, 0.0f,   0.0f,0.0f,0.0f,
+
+		// Octava línea
+		-0.45f,  0.21f, 0.0f,   0.0f,0.0f,0.0f,
+		-0.40f,  0.21f, 0.0f,   0.0f,0.0f,0.0f,
+		-0.35f,  0.21f, 0.0f,   0.0f,0.0f,0.0f,
+		-0.30f,  0.21f, 0.0f,   1.0f,1.0f,1.0f,
+		-0.25f,  0.21f, 0.0f,   0.0f,0.0f,0.0f,
+		-0.20f,  0.21f, 0.0f,   1.0f,1.0f,1.0f,
+		-0.15f,  0.21f, 0.0f,   1.0f,1.0f,1.0f,
+		-0.10f,  0.21f, 0.0f,   0.0f,0.0f,0.0f,
+
+		// Novena línea
+		-0.40f,  0.14f, 0.0f,   0.0f,0.0f,0.0f,
+		-0.35f,  0.14f, 0.0f,   0.0f,0.0f,0.0f,
+		-0.30f,  0.14f, 0.0f,   0.0f,0.0f,0.0f,
+		-0.25f,  0.14f, 0.0f,   0.0f,0.0f,0.0f,
+		-0.20f,  0.14f, 0.0f,   1.0f,1.0f,1.0f,
+		-0.15f,  0.14f, 0.0f,   0.0f,0.0f,0.0f,
+
+		// Décima línea
+		-0.35f,  0.07f, 0.0f,   0.0f,0.0f,0.0f,
+		-0.30f,  0.07f, 0.0f,   0.0f,0.0f,0.0f,
+		-0.25f,  0.07f, 0.0f,   0.0f,0.0f,0.0f,
+		-0.20f,  0.07f, 0.0f,   0.0f,0.0f,0.0f,
+
 	};
-	unsigned int indices[] = {  // note that we start from 0!
-		3,2,1,// second Triangle
-		0,1,3,
-		
-	};
 
 
-
-	GLuint VBO, VAO,EBO;
+	GLuint VBO, VAO;
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
-	glGenBuffers(1, &EBO);
 
 	// Enlazar  Vertex Array Object
 	glBindVertexArray(VAO);
@@ -89,18 +174,15 @@ int main() {
 	//2.- Copiamos nuestros arreglo de vertices en un buffer de vertices para que OpenGL lo use
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-	// 3.Copiamos nuestro arreglo de indices en  un elemento del buffer para que OpenGL lo use
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
 	// 4. Despues colocamos las caracteristicas de los vertices
 
 	//Posicion
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid *)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)0);
 	glEnableVertexAttribArray(0);
 
 	//Color
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid *)(3*sizeof(GLfloat)));
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
 	glEnableVertexAttribArray(1);
 
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
@@ -109,7 +191,7 @@ int main() {
 	glBindVertexArray(0); // Unbind VAO (it's always a good thing to unbind any buffer/array to prevent strange bugs)
 
 
-	
+
 	while (!glfwWindowShouldClose(window))
 	{
 		// Check if any events have been activiated (key pressed, mouse moved etc.) and call corresponding response functions
@@ -117,28 +199,21 @@ int main() {
 
 		// Render
 		// Clear the colorbuffer
-		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+		glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
 
 		// Draw our first triangle
-        ourShader.Use();
-        glBindVertexArray(VAO);
+		ourShader.Use();
+		glBindVertexArray(VAO);
 
 
-        glPointSize(1);
-        glDrawArrays(GL_POINTS,0,1);
-        
-        //glDrawArrays(GL_LINES,0,2);
-        //glDrawArrays(GL_LINE_LOOP,0,4);
-        
-        //glDrawArrays(GL_TRIANGLES,0,3);
-        //glDrawElements(GL_TRIANGLES, 3,GL_UNSIGNED_INT,0);
+		glPointSize(15);
+		glDrawArrays(GL_POINTS, 0, 76);
 
-        
-        
-        glBindVertexArray(0);
-    
+
+		glBindVertexArray(0);
+
 		// Swap the screen buffers
 		glfwSwapBuffers(window);
 	}
